d0389ff8c97244e5711d7e13af9b2bfcd516d32d
Pol Milian <polcius@gmail.com>
2018-11-06 12:14:18 +1030

    lint

--- src/templates/index.js
+++ src/templates/index.js
@@ -9,6 +9,7 @@ import Pagination from '../components/Pagination'
 import SEO from '../components/SEO'
 import config from '../utils/siteConfig'
 
+
 const Index = ({ data, pageContext }) => {
   const posts = data.allContentfulPost.edges
   const featuredPost = posts[0].node
