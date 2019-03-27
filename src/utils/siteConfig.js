23a37f75f2be55b1541df398bef30f3d00fa4069
Pol Milian <polcius@gmail.com>
2019-03-27 19:25:12 +0800

    updated metadata

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,12 +3,12 @@ module.exports = {
   siteTitleAlt: 'Bulenya - The Blog', // This allows an alternative site title for SEO schema.
   publisher: 'Pol Milian', // Organization name used for SEO schema
   siteDescription:
-    'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
+    'Bulenya ("The Stranger") is a blog written by Pol Milian about fear & love, travel, food and web development',
   siteUrl: 'https://bulenya.xyz', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
   author: 'Pol Milian', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  authorUrl: 'https://twitter.com/e_l_pol/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@EL_Pol', // Change for Twitter Cards
   shortTitle: 'Bulenya', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/logo-512.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
