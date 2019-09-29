21b5f8db9489351c662f6d88ee5291da6c5b57b6
Pol Milian <polcius@gmail.com>
2019-09-29 18:10:57 +1000

    Revert "removed styles (test)"
    This reverts commit 9844ac5e24cbef8703a19f4b79fc3a8887829106.

--- src/components/PageBody.js
+++ src/components/PageBody.js
@@ -6,9 +6,62 @@ const Body = styled.div`
   margin: 0 auto;
   max-width: ${props => props.theme.sizes.maxWidthCentered};
 
+  h1,
+  h2,
+  h3 {
+    font-weight: 600;
+    line-height: 1.25;
+    margin: 0 0 1rem 0;
+    text-transform: capitalize;
+  }
+
+  h1 {
+    font-size: 1.5em;
+  }
+  h2 {
+    font-size: 1.25em;
+  }
+  h3 {
+    font-size: 1em;
+  }
+
+  p {
+    line-height: 1.6;
+    margin: 0 0 2em 0;
+  }
+
   a {
     transition: 0.2s;
     color: ${props => props.theme.bodyLink}
+  }
+
+  del {
+    text-decoration: line-through;
+  }
+  strong {
+    font-weight: 600;
+  }
+  em {
+    font-style: italic;
+  }
+
+  ul,
+  ol {
+    margin: 0 0 2em 0;
+  }
+
+  ul {
+    li {
+      list-style: disc;
+      line-height: 1.25;
+    }
+  }
+
+  ol {
+    li {
+      list-style: decimal;
+      line-height: 1.25;
+    }
   }
 
   hr {
