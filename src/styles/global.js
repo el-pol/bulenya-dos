9844ac5e24cbef8703a19f4b79fc3a8887829106
Pol Milian <polcius@gmail.com>
2019-09-29 12:25:31 +1000

    removed styles (test)

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,28 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  /* http://meyerweb.com/eric/tools/css/reset/
-   v2.0 | 20110126
-   License: none (public domain)
-  */
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    vertical-align: baseline;
-  }
 
   /* Added to Fix Footer to bottom of viewport */
   html, body {
@@ -44,70 +22,5 @@ const GlobalStyle = createGlobalStyle`
 
   /* End Fix to Place Footer on Bottom of Viewport */
 
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-
-  @media screen and (min-width: 35em) {
-    html {
-      margin-right: calc(-100vw + 100%);
-      overflow-x: hidden;
-    }
-  }
-
-  ol, ul, li {
-    list-style: none;
-  }
-
-  blockquote, q {
-    quotes: none;
-  }
-
-  blockquote::before, blockquote::after,
-  q::before, q::after {
-    content: '';
-    content: none;
-  }
-
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    background: white;
-    text-rendering: optimizeLegibility;
-    text-shadow: rgba(0, 0, 0, .01) 0 0 1px;
-  }
-
-  img {
-    display: block;
-  	width: 100%;
-  	height: auto;
-  }
-
-  a {
-    color: #2457ec;
-  }
-
-  button,
-  input {
-    border: none;
-    outline: none;
-    appearance: none;
-    border-radius: 0;
-    resize: none;
-    &:focus {
-      outline: none;
-    }
-    &:invalid {
-      box-shadow: none;
-    }
-  }
 `
 export default GlobalStyle
