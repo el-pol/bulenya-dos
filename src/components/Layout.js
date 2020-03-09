1974d4c435d111f655869279dc36dcf03f5dd6d0
Pol Milian <polcius@gmail.com>
2020-03-09 14:01:29 +1100

    restored global style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,8 +18,8 @@ const Template = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={favicon} />
       </Helmet>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>
-      {/* <GlobalStyle /> */}
         <>
           <div className="siteContent">
             <Menu />
